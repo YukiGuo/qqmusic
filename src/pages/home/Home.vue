c6d5adf1f14ae201b9be4459143f8ced87e000e9
Yuki <834517986@qq.com>
2018-11-25 11:56:31 +0800

    songlist静态页面的实现

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <home-head></home-head>
-      <!--   <song-list :list = "playlist"></song-list> -->
+        <song-list :list = "playlist"></song-list>
     </div>
 </template>
 <script >
 import HomeHead from './components/Head'
-// import SongList from './components/SongList'
-// import axios from 'axios'
+import SongList from './components/SongList'
+import axios from 'axios'
 export default {
   name: 'Home',
   data () {
@@ -17,25 +17,23 @@ export default {
   },
   components: {
     HomeHead,
-    // SongList
-  }
-  /*methods: {
+    SongList
+  },
+  methods: {
     getHomeInfo () {
       axios.get('/static/mock/data.json').then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
       res = res.data
-      if(res.ret && res.data ) {
+      if (res.ret && res.data) {
         const data = res.data
         this.playlist = data.playlist
       }
-
-
     }
   },
   mounted () {
     this.getHomeInfo()
-  }*/
+  }
 }
 </script>
 
---
+++ src/pages/home/components/SongList.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="wrapper">
+    <h1>歌单推荐</h1>
+    <div class="menu">
+      <ul>
+        <li class="sec1-nav">为你推荐</li>
+        <li class="sec1-nav">网络歌曲</li>
+        <li class="sec1-nav">经典</li>
+        <li class="sec1-nav">夜店</li>
+        <li class="sec1-nav">情感</li>
+        <li class="sec1-nav">KTV热歌</li>
+      </ul>
+    </div>
+    <div class="listwrapper">
+      <ul
+      v-for= '(item) of list'
+      v-bind:key = item.id
+      >
+      <!-- <swiper :options="swiperOption" > -->
+        <!-- <swiper-slide> -->
+          <li class="box"><img :src = item.url><br>
+            <span class="title">{{item.title}} </span><br>
+            <span class="playnum">播放量： {{item.playNum}}</span>
+          </li>
+<!--         </swiper-slide>  
+        <div class="swiper-pagination"  slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+      </swiper> -->
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script >
+export default {
+  name: 'SongList',
+  props: {
+    list: Array
+  }
+  // data: function() {
+  //   return{
+  //     swiperOption: {}
+  //   }
+  // }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~@/assets/style/varibles.styl'
+.wrapper
+  padding:30px 0 10px 0
+  height:100px
+  background-color:#ddd
+  h1
+    line-height:20px
+    font-size:20px
+    text-align:center
+    margin:auto
+  .menu
+    text-align:center
+    li
+      line-height:50px
+      display:inline-block
+      padding:20px
+      font-size:15px
+    li:first-child
+      color:$bgColor
+  .listwrapper
+    .box
+      width:230px
+      float:left
+      padding:0 10px
+      img
+        width:100% 
+        padding-bottom:10px 
+      .title
+      
+        line-height:25px
+      .playnum 
+         font-size:13px
+         line-height:25px
+         color:#bbb      
+
+</style>
